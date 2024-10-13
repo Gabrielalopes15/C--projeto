eaadb63389ed1c3ee608c52e8a54fd57cb49e61c
Gabriela lopes <gabylopes2000@gmail.com>
2024-10-13 14:32:35 -0300

    Update main.h

--- main.h
+++ main.h
@@ -1,19 +1,17 @@
-#include<string.h>
-#define MAX_TAM 100     // Define um tamanho máximo para os campos de texto
+#include <string.h>
 
-#ifndef MAIN_H
-#define MAIN_H
+#define MAX_TAM 100
+
 
 // Definição de uma estrutura que representa um chamado
 typedef struct {
+    char valor[MAX_TAM];
     int id;                // ID do chamado
     char nome[80];     // Nome do cliente
     char telefone[15]; // Telefone do cliente
     char email[100];    // Email do cliente
     char texto[MAX_TAM];    // Texto do chamado (a dúvida ou questão do cliente)
-    char status[15];   // Status do chamado (ex.: "ABERTO", "PENDENTE" ,"FECHADO")
+    char status[MAX_TAM];   // Status do chamado (ex.: "ABERTO", "PENDENTE" ,"FECHADO")
     char resolucao[MAX_TAM]; // Resolução do chamado (se houver)
-    int senha; //senha do tecnico
+    char cpf[MAX_TAM];
 } Chamado;
-
-#endif // MAIN_H
