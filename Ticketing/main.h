265bdd0accd9c8302212419a5211c9e79b6139a7
Gabriela Lopes <gabylopes2000@gmail.com>
2024-11-24 18:07:27 -0300

    Update main.h

--- Ticketing/main.h
+++ Ticketing/main.h
@@ -4,12 +4,29 @@
 
 #include <sqlite3.h>
 
-// PrototipaÁ„o das funÁıes
-int abrirBanco(sqlite3 **db);
-int criarTabela(sqlite3 *db);
-int cpfExiste(sqlite3 *db, const char *cpf);
-int cadastrarCliente(sqlite3 *db, const char *nome, const char *tel, const char *cpf);
-int countClientes();
+// Defini√ß√£o da estrutura Chamado
+typedef struct {
+    int id;
+    char descricao[256];
+    char status[50];
+
+    // Adicione outros campos conforme necess√°rio
+} Chamado;
+
+// Declara√ß√µes de fun√ß√µes
+void setColor(int textColor, int bgColor);
+void imprime_cabec(void);
+void limpa(void);
+void bemvindo(void);
+void saindo(void);
+void carregando(void);
+void grafico_clientes(void);
+void imprime_menu(void);
+void registrarChamado(Chamado *chamado, int id);
+void exibirChamado(Chamado *chamado);
+void escolher_imprimir(void);
+void consultarChamadoPorID(Chamado **chamados, int numChamados, int consultaID);
+void login_tecnico(Chamado **chamados, int numChamados, int consultaID);
+void graficoChamados(sqlite3 *db);
 
 #endif // MAIN_H
-
